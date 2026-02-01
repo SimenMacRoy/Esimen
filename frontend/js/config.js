34c5861aac560a94e6df8594a7d58d678cea2435
Mac Roy Tchuileng Simen <Mac.Roy.Tchuileng.Simen@uqtr.ca>
2026-02-01 18:32:31 -0500

    Fix issues

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -5,7 +5,7 @@
 // En production: 'https://votre-app.railway.app' (URL fournie par Railway)
 // ============================================
 const config = {
-    baseURL: 'http://localhost:3006',
+    baseURL: 'https://esimen-production.up.railway.app',
 
     // Stripe public key (la clé de test pour le dev, clé live pour prod)
     stripePublicKey: 'pk_test_51PIRk7DIrmiE2Hgb4lLVD99VQnFg7uWaAhtEBBBzLIixaLhcQ9FOuhkSonPw8SozcgiS19efR92rNwYX6kQ7TRvT00YayxN2sq'
